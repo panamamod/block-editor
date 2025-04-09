880a822f5ed50e44fc877034c1992463d4164b1b
Andrey Vedilin <vvv@mail.ru>
2025-04-10 01:13:23 +0300

    добавил иконку текстового блока

--- dist/main.js
+++ dist/main.js
@@ -809,7 +809,7 @@ BlockEditorPlugin.BLOCK_TYPE_OPTIONS = [
     {
         type: 'p',
         label: 'Text block',
-        icon: '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 12h.01"/><path d="M10 16H7a4 4 0 1 1 0-8h10a4 4 0 0 0-4 8h-2"/></svg>'
+        icon: '<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="4" y1="6" x2="20" y2="6"/><line x1="4" y1="12" x2="14" y2="12"/><line x1="4" y1="18" x2="18" y2="18"/></svg>'
     },
     {
         type: 'h1',
